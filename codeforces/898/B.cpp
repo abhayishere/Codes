88b88e19fa8607a538dc9ce9c89dc7d65f201ff0
Abhay Yadav <abhyadav1@gmail.com>
2020-07-02 06:59:00 +0530

    Add solution for problem `B - Proper Nutrition` Link: https://codeforces.com/contest/898/problem/B Tags: brute force, implementation, number theory, *1100 Ref: https://codeforces.com/contest/898/submission/85726876

--- codeforces/898/B.cpp
+++ codeforces/898/B.cpp
@@ -1,70 +1,30 @@
-/*	[]__________[][][][]____[][][][][]__{}__[][][][]____[][][][][]__[][][][]
-    []__________[]______________[]__________[]__________[]__________[]____[]
-    []__________[][][][]________[]__________[][][][]____[]__________[]____[]
-    []__________[]______________[]________________[]____[]__[][][]__[]____[]
-    []__________[]______________[]________________[]____[]______[]__[]____[]
-    [][][][]____[][][][]________[]__________[][][][]____[][][][][]__[][][][]
-*/
-#include <bits/stdc++.h>
-#define int long long int
-#define mod 1000000007
-#define endl '\n'
+#include<bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#define decimal(x) cout<<fixed<<setprecision(x);
-bool prime[100005]; 
-void Sieve(int n) {
-    memset(prime, true, sizeof(prime)); 
-  
-    for (int p=2; p*p<=n; p++) 
-    { 
-        if (prime[p] == true) 
-        { 
-            for (int i=p*p; i<=n; i += p) 
-                prime[i] = false; 
-        } 
-    } 
-  
-} 
-vector<int>grapj[100005];
-bool vis[100005];
-void solve()
+typedef long long int ll;
+int main()
 {
     int n,a,b;
     cin>>n>>a>>b;
-    int x=-1,y=-1;
-    for(int i=0;i<=n/a;i++)
+    int x=-1,y=-1,i;
+    for(i=0;i<=(n/a);i++)
     {
-        if((n-a*i)%b==0)
+        if((n-i*a)%b==0)
         {
-            x=i,y=(n-a*i)/b;
+            x=i;
+            y=(n-i*a)/b;
             break;
         }
     }
-    if(x==-1 && y==-1)
+    if(x!=-1 && y!=-1)
     {
-        cout<<"NO"<<endl;
+        cout<<"YES"<<endl;
+        cout<<i<<" "<<(n-i*a)/b;
     }
     else
     {
-        cout<<"YES"<<endl;
-        cout<<x<<" "<<y<<endl;
+        cout<<"NO";
     }
     
-}	
-signed main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-#endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t=1;
-       //cin>>t;
-    while(t--)
-    {
-        solve();
-    }
+    cout<<endl;
 }
+//x*a+y*b=n
