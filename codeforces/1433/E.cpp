edd8951e99e4f456e7e2e03f049be9d6cc2fa38a
Abhay Yadav <abhyadav1@gmail.com>
2020-10-20 22:10:00 +0530

    Add solution for problem `E - Two Round Dances` Link: https://codeforces.com/contest/1433/problem/E Tags: combinatorics, math, *1300 Ref: https://codeforces.com/contest/1433/submission/96166651

---
+++ codeforces/1433/E.cpp
@@ -0,0 +1,66 @@
+/*	[]__________[][][][]____[][][][][]__{}__[][][][]____[][][][][]__[][][][]
+    []__________[]______________[]__________[]__________[]__________[]____[]
+    []__________[][][][]________[]__________[][][][]____[]__________[]____[]
+    []__________[]______________[]________________[]____[]__[][][]__[]____[]
+    []__________[]______________[]________________[]____[]______[]__[]____[]
+    [][][][]____[][][][]________[]__________[][][][]____[][][][][]__[][][][]
+*/
+#include <bits/stdc++.h>
+#define int long long int
+#define mod 1000000007
+#define endl '\n'
+using namespace std;
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#define decimal(x) cout<<fixed<<setprecision(x);
+bool prime[100005]; 
+void Sieve(int n) {
+    memset(prime, true, sizeof(prime)); 
+  
+    for (int p=2; p*p<=n; p++) 
+    { 
+        if (prime[p] == true) 
+        { 
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+  
+} 
+int fact(int n); 
+  
+int ncr(int n, int r) 
+{ 
+    return fact(n) / (fact(r) * fact(n - r)); 
+} 
+int fact(int n) 
+{ 
+    int res = 1; 
+    for (int i = 2; i <= n; i++) 
+        res = res * i; 
+    return res; 
+} 
+void solve()
+{
+    int n;
+    cin>>n;
+    int ans=1;
+    ans=ncr(n,n/2);
+    for(int i=1;i<n/2;i++)ans=ans*i*i;
+    cout<<ans/2<<endl;
+}	
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+       //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
