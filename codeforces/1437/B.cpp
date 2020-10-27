ab6dde3206fb38105a6d7dea54b1120867fa9c47
Abhay Yadav <abhyadav1@gmail.com>
2020-10-27 20:21:00 +0530

    Add solution for problem `B - Reverse Binary Strings` Link: https://codeforces.com/contest/1437/problem/B Tags: constructive algorithms, greedy, *1200 Ref: https://codeforces.com/contest/1437/submission/96877438

---
+++ codeforces/1437/B.cpp
@@ -0,0 +1,66 @@
+/*	[]__________[][][][]____[][][][][]__{}__[][][][]____[][][][][]__[][][][]
+    []__________[]______________[]__________[]__________[]__________[]____[]
+    []__________[][][][]________[]__________[][][][]____[]__________[]____[]
+    []__________[]______________[]________________[]____[]__[][][]__[]____[]
+    []__________[]______________[]________________[]____[]______[]__[]____[]
+    [][][][]____[][][][]________[]__________[][][][]____[][][][][]__[][][][]
+*/
+#include <bits/stdc++.h>
+#define int long long int
+#define mod 1000000007
+#define endl '\n'
+using namespace std;
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#define decimal(x) cout<<fixed<<setprecision(x);
+bool prime[100005]; 
+void Sieve(int n) {
+    memset(prime, true, sizeof(prime)); 
+  
+    for (int p=2; p*p<=n; p++) 
+    { 
+        if (prime[p] == true) 
+        { 
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+  
+} 
+vector<int>grapj[100005];
+bool vis[100005];
+void solve()
+{
+    int f=0,c=1;
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=1;i<n;i++)
+    {
+        if(s[i]==s[i-1])
+        c++;
+        else
+        {
+            if(c>1)
+            f=f+c-1;
+        }
+    }
+    c=c/2;
+    cout<<c<<endl;
+}	
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+       cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
