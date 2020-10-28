a6587917c56b92a329ebd70f0afe866a5d3f9c3f
Abhay Yadav <abhyadav1@gmail.com>
2020-10-28 13:36:00 +0530

    Add solution for problem `A - Death Note` Link: https://codeforces.com/contest/1016/problem/A Tags: greedy, implementation, math, *900 Ref: https://codeforces.com/contest/1016/submission/96973842

---
+++ codeforces/1016/A.cpp
@@ -0,0 +1,61 @@
+/*	[]__________[][][][]____[][][][][]__{}__[][][][]____[][][][][]__[][][][]
+    []__________[]______________[]__________[]__________[]__________[]____[]
+    []__________[][][][]________[]__________[][][][]____[]__________[]____[]
+    []__________[]______________[]________________[]____[]__[][][]__[]____[]
+    []__________[]______________[]________________[]____[]______[]__[]____[]
+    [][][][]____[][][][]________[]__________[][][][]____[][][][][]__[][][][]
+*/
+#include <bits/stdc++.h>
+#define int long long int
+#define mod 1000000007
+#define endl '\n'
+using namespace std;
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#define decimal(x) cout<<fixed<<setprecision(x);
+bool prime[100005]; 
+void Sieve(int n) {
+    memset(prime, true, sizeof(prime)); 
+  
+    for (int p=2; p*p<=n; p++) 
+    { 
+        if (prime[p] == true) 
+        { 
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+  
+} 
+vector<int>grapj[100005];
+bool vis[100005];
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    int arr[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    int lst=0,ans=0,la=0;
+    for(int i=0;i<n;i++)
+    {
+        lst+=arr[i];
+        ans=lst/m;
+        lst=lst%m;
+        cout<<ans<<" ";
+    }
+}	
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+       //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
