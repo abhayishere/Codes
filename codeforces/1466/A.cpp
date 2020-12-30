8b452afcc169722837bc6a5a44f4522316d186da
Abhay Yadav <abhyadav1@gmail.com>
2020-12-30 20:09:00 +0530

    Add solution for problem `A - Bovine Dilemma` Link: https://codeforces.com/contest/1466/problem/A Tags: brute force, geometry, math, *800 Ref: https://codeforces.com/contest/1466/submission/102776131

---
+++ codeforces/1466/A.cpp
@@ -0,0 +1,61 @@
+//karne se hi hota hai ;)
+#include <bits/stdc++.h>
+#define int long long int
+#define mod 1000000007
+#define endl '\n'
+#define fr(i,a,b) for(int i=a;i<b;i++)
+#define sortv(a) sort(a.begin(),a.end())
+#define sorta(a,n) sort(a,a+n)
+#define bgnd(v) v.begin(),v.end()
+#define pb push_back
+#define mp make_pair
+#define inf 1e16
+#define lb lower_bound  //THIS GIVES THE ITR TO THE ELEMENT IN RANGE [SI,EI) THAT IS >= OUR VALUE 
+#define ub upper_bound  //THIS GIVES THE ITR TO THE ELEMENT IN RANGE [SI,EI) THAT IS > OUR VALUE 
+using namespace std;
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#define decimal(x) cout<<fixed<<setprecision(x);
+bool prime[100005]; 
+void Sieve(int n) {
+    memset(prime, true, sizeof(prime)); 
+    for (int p=2; p*p<=n; p++) 
+    { 
+        if (prime[p] == true) 
+        { 
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+} 
+void solve()
+{
+    map<double,int>mp;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            double d=(1.0*abs(arr[i]-arr[j]))/2;
+            mp[d]++;
+        }
+    }cout<<mp.size()-1<<endl;
+}
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
